93750d1900927e395002693d50c75ee2aaae5e2b
Bolao Admin <admin@bolao.com>
2026-05-27 16:05:53 -0300

    fix: import redirect in actions

--- src/app/groups/[id]/actions.ts
+++ src/app/groups/[id]/actions.ts
@@ -2,6 +2,7 @@
 
 import { createClient } from '@/utils/supabase/server'
 import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
 export async function saveGuess(formData: FormData) {
   const supabase = await createClient()
